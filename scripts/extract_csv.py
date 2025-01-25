694b10b3c47983b83b7a0390314bc264885c3974
thallyssonklein <thallyssonklein@gmail.com>
2025-01-25 20:36:49 -0300

    refact: creating scripts folder

---
+++ scripts/extract_csv.py
@@ -0,0 +1,31 @@
+import csv
+
+def extrair(arquivo_origem, arquivo_destino):
+    colunas = ["Compra", "Data", "Descrição", "Parcela", "R$", "US$"]
+    linhas_ifood = []
+    linhas_restantes = []
+
+    with open(arquivo_origem, "r", encoding="utf-8") as f:
+        leitor = csv.DictReader(f)
+        for linha in leitor:
+            if None in linha.keys():
+                continue
+            if "iFood" in linha["Descrição"]:
+                linhas_ifood.append(linha)
+            else:
+                linhas_restantes.append(linha)
+
+
+    with open(arquivo_destino, "w", newline="", encoding="utf-8") as f_dest:
+        escritor = csv.DictWriter(f_dest, fieldnames=colunas)
+        escritor.writeheader()
+        escritor.writerows(linhas_ifood)
+
+    with open(arquivo_origem, "w", newline="", encoding="utf-8") as f_orig:
+        escritor = csv.DictWriter(f_orig, fieldnames=colunas)
+        escritor.writeheader()
+        escritor.writerows(linhas_restantes)
+
+# Exemplo de uso:
+extrair("./test.csv",
+              "./ifood.csv")
